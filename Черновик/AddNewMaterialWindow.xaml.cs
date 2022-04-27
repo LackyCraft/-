5af2b24f6b3bcafb0c6b59058c940bc3800c6f00
Dany <zqxw1245@mail.ru>
2022-04-27 12:35:28 +0300

    firstCommit

---
+++ Черновик/AddNewMaterialWindow.xaml.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Input;
+using System.Windows.Media;
+using Microsoft.Win32;
+
+namespace Черновик
+{
+    /// <summary>
+    /// Логика взаимодействия для AddNewMaterialWindow.xaml
+    /// </summary>
+    public partial class AddNewMaterialWindow : Window
+    {
+
+        public AddNewMaterialWindow()
+        {
+            InitializeComponent();
+            List<string> unitList = new List<string>() { "кг", "л", "м" };
+            ComboBoxUnit.ItemsSource = unitList;
+
+            ComboBoxMaterialType.ItemsSource = DataBase.DraftDataBaseEntity.GetContext().MaterialType.Where(i => i.ID != -1).ToList();
+            ComboBoxSupplier.ItemsSource = DataBase.DraftDataBaseEntity.GetContext().Supplier.ToList();
+        }
+
+        private void selectedFoto(object sender, RoutedEventArgs e)
+        {
+
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Select a picture";
+            op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+              "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (*.png)|*.png";
+
+            if (op.ShowDialog() == true)
+            {
+                FotoMaterial.Source = new BitmapImage(new Uri(op.FileName));
+                TextBoxFotoName.Text = op.FileName;
+            }
+        }
+
+        private void cLickButtonAddSupplier(object sender, RoutedEventArgs e)
+        {
+            DataGridSuppliersList.Items.Add(ComboBoxSupplier.SelectedItem as DataBase.Supplier);
+        }
+
+        private void DeletedAt(object sender, RoutedEventArgs e)
+        {
+            DataGridSuppliersList.Items.Remove(DataGridSuppliersList.SelectedItem as DataBase.Supplier);
+        }
+
+        private void clickAddNewMaterial(object sender, RoutedEventArgs e)
+        {
+            string MessageError = "";
+
+            int checkCount;
+            if (!int.TryParse(TextBoxCountInPack.Text, out checkCount))
+            {
+                MessageError += "\nКоличество шт в упакове может быть только целым положительным числом!";
+            }
+            if (!int.TryParse(TextBoxCountInStock.Text, out checkCount))
+            {
+                MessageError += "\nОстаток на складе может быть только целым положительным числом!";
+            }
+            if (!int.TryParse(TextBoxMinCount.Text, out checkCount))
+            {
+                MessageError += "\nМинимальное количество может быть только целым положительным числом!";
+            }
+            if (TextBoxDesc.Text.Length < 2)
+            {
+                MessageError += "\nЗаполните описание!";
+            }
+            if (!int.TryParse(TextBoxCoast.Text, out checkCount))
+            {
+                MessageError += "\nСтоимость может быть только целым положительным числом!";
+            }
+            if(ComboBoxUnit.SelectedIndex == -1)
+            {
+                MessageError += "\nВыберите ед. измерения!";
+            }
+            if (ComboBoxMaterialType.SelectedIndex == -1)
+            {
+                MessageError += "\nВыберите тип материала!";
+            }
+            if (TextBoxFotoName.Text == "Выберите фотографию")
+            {
+                MessageError += "\n Выберите фотографию или оставьте фото по умолчанию";
+            }
+            if (MessageError.Length > 2)
+            {
+                MessageBox.Show(MessageError);
+            }
+            else
+            {
+                try
+                {
+                    DataBase.Material newMaterials = new DataBase.Material();
+                    newMaterials.Title = TextBoxNameMaterial.Text;
+                    newMaterials.CountInPack = int.Parse(TextBoxCountInPack.Text);
+                    newMaterials.Unit = ComboBoxUnit.SelectedValue.ToString();
+                    newMaterials.CountInStock = int.Parse(TextBoxCountInStock.Text);
+                    newMaterials.MinCount = int.Parse(TextBoxMinCount.Text);
+                    newMaterials.Description = TextBoxDesc.Text;
+                    newMaterials.Cost = int.Parse(TextBoxCoast.Text);
+                    newMaterials.Image = TextBoxFotoName.Text;
+                    newMaterials.MaterialTypeID = int.Parse(ComboBoxMaterialType.SelectedValue.ToString());
+                    DataBase.DraftDataBaseEntity.GetContext().Material.Add(newMaterials);
+                    DataBase.DraftDataBaseEntity.GetContext().SaveChanges();
+
+                    if (DataGridSuppliersList.Items.Count > 0)
+                    {
+                        for (int i = 0; i < DataGridSuppliersList.Items.Count; i++)
+                        {
+                            //DataBase. newMaterials = new DataBase.Material();
+                            newMaterials.Supplier.Add(DataGridSuppliersList.Items[i] as DataBase.Supplier);
+                        }
+                        DataBase.DraftDataBaseEntity.GetContext().SaveChanges();
+                        MessageBox.Show("Запись была успешно добавлена в БД");
+                        this.Close();
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Произошла непредвиденная ошибка");
+                    
+                }
+
+
+    }
+
+
+
+
+        }
+    }
+}
---
+++ Черновик/DraftMainWindow.xaml.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Черновик
+{
+    public partial class DraftMainWindow : Window
+    {
+        //ЛИСТ (копия) БД - будет хранится в памяти
+        private List<DataBase.MaterialList> AllMaterialList;
+
+        //Лист, который будет для сортировки датагрида в реальном времени
+        private List<DataBase.MaterialList> gridMaterialList;
+
+        //Размер одной страницы 15 - строк
+        private int PageSize = 15;
+
+        public DraftMainWindow()
+        {
+            InitializeComponent();
+
+            //заполняем комбобок фильтрации типами материалов
+            ComboBoxFilter.ItemsSource = DataBase.DraftDataBaseEntity.GetContext().MaterialType.ToList();
+
+            //заполняем комбобок сортировки типами для сортировки
+            List<string> sortList = new List<string>() { "Нимаенование", "Остаток на складе", "Стоимость"};
+            ComboBoxSort.ItemsSource = sortList;
+
+            //заполняем листы, пока что полными слепками БД
+            AllMaterialList = DataBase.DraftDataBaseEntity.GetContext().MaterialList.ToList();
+
+            ComboBoxFilter.SelectedIndex = 0;
+
+        }
+
+
+        //Функция для перестройки dataGrid в реальном времени
+        public void drawDataGrid()
+        {
+            gridMaterialList = AllMaterialList;
+            int selectedTypeMAterialId = int.Parse(ComboBoxFilter.SelectedValue.ToString());
+
+            if (selectedTypeMAterialId != -1)
+            {
+                gridMaterialList = gridMaterialList.Where(i => i.MaterialTypeID == selectedTypeMAterialId).ToList();
+            }
+            if (ComboBoxSort.SelectedIndex !=-1)
+            {
+                if (ComboBoxSort.SelectedIndex == 0)
+                {
+                    gridMaterialList = gridMaterialList.OrderBy(i => i.Title).ToList();
+                }else if(ComboBoxSort.SelectedIndex == 1)
+                {
+                    gridMaterialList = gridMaterialList.OrderBy(i => i.CountInStock).ToList();
+                }else if (ComboBoxSort.SelectedIndex == 2)
+                {
+                    gridMaterialList = gridMaterialList.OrderBy(i => i.Cost).ToList();
+                }
+            }
+            if (TextBoxSearch.Text.Length > 1)
+            {
+                gridMaterialList = gridMaterialList.Where(i => i.Title.Contains(TextBoxSearch.Text) || i.Description.Contains(TextBoxSearch.Text)).ToList();
+            }
+
+            TextBoxStatusLine.Text = "Найдено: " + gridMaterialList.Count.ToString() + " из " + AllMaterialList.Count.ToString() + " записей в БД";
+
+            //Получаем количество страниц
+            TextBoxCountPage.Text = ((gridMaterialList.Count + PageSize - 1) / PageSize).ToString();
+
+            //Считаем сколько, строк надо пропустить до выбранной страницы.
+            int skipSize = (int.Parse(TextBoxPageNumber.Text)-1)*PageSize;
+
+            //Заполняем грид 15-ю записями с нужной нам страницы
+            DataGridMaterialList.ItemsSource = gridMaterialList.Skip(skipSize).Take(PageSize);
+            DataGridMaterialList.CanUserAddRows = false;
+        }
+
+        private void selectionChangedComboBox(object sender, SelectionChangedEventArgs e)
+        {
+            //Если менялись фильтры, скидываем пользователя на первую страницу
+            TextBoxPageNumber.Text = "1";
+            drawDataGrid();
+        }
+
+        //Реализация пагинации
+        private void clickButtonPage(object sender, RoutedEventArgs e)
+        {
+            if(sender == ButtonNextPage && int.Parse(TextBoxPageNumber.Text)<int.Parse(TextBoxCountPage.Text))
+            {
+                TextBoxPageNumber.Text = (int.Parse(TextBoxPageNumber.Text) + 1).ToString();
+                drawDataGrid();
+            }
+            if (sender == ButtonBackPage && int.Parse(TextBoxPageNumber.Text) > 1)
+            {
+                TextBoxPageNumber.Text = (int.Parse(TextBoxPageNumber.Text) - 1).ToString();
+                drawDataGrid();
+            }
+        }
+
+        //Перенаправление при поиске на обновление DataGrid
+        private void textChangedTextBoxSearch(object sender, TextChangedEventArgs e)
+        {
+            TextBoxPageNumber.Text = "1";
+            drawDataGrid();
+        }
+
+        //Обработка отчистки фильтра, поиска, и сортирвоки
+        private void clickButtonClear(object sender, RoutedEventArgs e)
+        {
+            ComboBoxSort.SelectedIndex = -1;
+            ComboBoxFilter.SelectedValue = -1;
+            TextBoxSearch.Text = "";
+        }
+
+        //Обработка выбора материалов для редактирования минимального количества
+        private void selectionChangedDataGridMaterialList(object sender, SelectionChangedEventArgs e)
+        {
+            if (DataGridMaterialList.SelectedItems.Count > 0)
+            {
+                TextBoxEditMinCount.IsEnabled = true;
+                ButtonEditMinCount.IsEnabled = true;
+                try
+                {
+                    double minCount = (DataGridMaterialList.SelectedItems[0] as DataBase.MaterialList).MinCount;
+                    for (int i = 1; i < DataGridMaterialList.SelectedItems.Count; i++)
+                    {
+                        if ((DataGridMaterialList.SelectedItems[i] as DataBase.MaterialList).MinCount < minCount)
+                            minCount = (DataGridMaterialList.SelectedItems[i] as DataBase.MaterialList).MinCount;
+                    }
+                    TextBoxEditMinCount.Text = minCount.ToString();
+                }
+                catch
+                {
+                    MessageBox.Show("Warning x0\nПроизошла непредвиденная ошибка");
+                }
+            }
+            else
+            {
+                TextBoxEditMinCount.IsEnabled = false;
+                ButtonEditMinCount.IsEnabled = false;
+            }
+        }
+
+        // Обработка нажатия ОК при быстром редактирвоании
+        private void clickButtonEditMinCount(object sender, RoutedEventArgs e)
+        {
+
+            if (MessageBox.Show("Вы правда хотите изменить у всех выделенных материалов минимальное количесвто на:" + TextBoxEditMinCount.Text + "?", "Измененить минимальное количесвто материала", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    for (int i = 0; i < DataGridMaterialList.SelectedItems.Count; i++)
+                    {
+                        int idEditMeterial = (DataGridMaterialList.SelectedItems[i] as DataBase.MaterialList).ID;
+                        DataBase.Material editMaterial = DataBase.DraftDataBaseEntity.GetContext().Material.Where(x => x.ID == idEditMeterial).First();
+                        editMaterial.MinCount = double.Parse(TextBoxEditMinCount.Text);
+                        DataBase.DraftDataBaseEntity.GetContext().SaveChanges();
+                    }
+                    AllMaterialList = DataBase.DraftDataBaseEntity.GetContext().MaterialList.ToList();
+                    drawDataGrid();
+                }
+                catch
+                {
+                    MessageBox.Show("Warning x0\nПроизошла непредвиденная ошибка");
+                }
+            }
+        }
+
+        // Контроль ввода минимального кол-ва шт материала при реадактировании
+        private void textChangedTextBoxEditMinCount(object sender, TextChangedEventArgs e)
+        {
+            int editMinCount=0;
+            if(!int.TryParse(TextBoxEditMinCount.Text, out editMinCount) || editMinCount<=0)
+            {
+                MessageBox.Show("Мнимальное количество шт, иожет быть только положительным целым цислом!");
+                TextBoxEditMinCount.Text = "1";
+            }
+        }
+
+        //Добавление нового метариала
+        private void clickButtonAddNewMaterial(object sender, RoutedEventArgs e)
+        {
+            (new AddNewMaterialWindow()).Show();
+            drawDataGrid();
+        }
+    }
+}
